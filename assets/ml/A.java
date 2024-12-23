bf5c9ecaa6f5fb88c5936db5eadf565f0802dc08
Patrick Baxter <baxterp159@gmail.com>
2024-12-22 20:27:19 -0500

    Simplified design.  Added model that is accurate. Needed to use labels.txt to correctly display label the model outputs

---
+++ assets/ml/A.java
@@ -0,0 +1,1 @@
+    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
--- lib/main.dart
+++ lib/main.dart
@@ -1,246 +1,165 @@
 import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:google_mlkit_image_labeling/google_mlkit_image_labeling.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image/image.dart' as img;
 import 'package:flutter/services.dart';
-import 'package:path/path.dart';
-import 'package:path_provider/path_provider.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ImageClassifierScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ImageClassifierScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ImageClassifierScreenState createState() => _ImageClassifierScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  File? image;
-  late ImagePicker imagePicker;
-  late ImageLabeler imageLabeler;
+class _ImageClassifierScreenState extends State<ImageClassifierScreen> {
+  File? _image;
+  List<double>? _output;
+  double _confidence = 0.0;
+  List<String> class_names = [];
+  bool _isClassifying = false;
+  String _predictedName = ""; // Store the predicted name
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    imagePicker = ImagePicker();
-    // ImageLabelerOptions options = ImageLabelerOptions(confidenceThreshold: 0.6);
-    // labeler = ImageLabeler(options: options);
-    loadModel();
+    loadClassNames();
   }
 
-  loadModel() async {
-    final modelPath = await getModelPath('assets/ml/fruits2.tflite');
-    final options = LocalLabelerOptions(
-      confidenceThreshold: 0.1,
-      modelPath: modelPath,
-    );
-    imageLabeler = ImageLabeler(options: options);
-  }
-
-  chooseImage() async {
-    XFile? selectedImage =
-        await imagePicker.pickImage(source: ImageSource.gallery);
-    if (selectedImage != null) {
-      image = File(selectedImage.path);
-      performImageLabeling();
-      setState(() {
-        image;
-      });
+  Future<void> loadClassNames() async {
+    try {
+      String data = await rootBundle.loadString('assets/ml/labels.txt');
+      class_names = data.split('\n').map((line) => line.trim()).toList(); // Trim whitespace
+      class_names.removeWhere((item) => item.isEmpty); // Remove empty lines
+      print("Loaded class names: $class_names"); // Print for debugging
+    } catch (e) {
+      print("Error loading class names: $e");
+      // Handle the error appropriately, e.g., display an error message to the user
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Error loading labels.txt")),
+      );
     }
   }
 
-  captureImage() async {
-    XFile? selectedImage =
-        await imagePicker.pickImage(source: ImageSource.camera);
-    if (selectedImage != null) {
-      image = File(selectedImage.path);
-      performImageLabeling();
-      setState(() {
-        image;
-      });
-    }
-  }
-
-  String resutls = '';
-  performImageLabeling() async {
-    resutls = "";
-    InputImage inputImage = InputImage.fromFile(image!);
-
-    final List<ImageLabel> labels = await imageLabeler.processImage(inputImage);
-
-    for (ImageLabel label in labels) {
-      final String text = label.label;
-      final int index = label.index;
-      final double confidence = label.confidence;
-      // print(text + "  " + confidence.toString());
-      print(text);
-      resutls += text + "  " + confidence.toStringAsFixed(2) + "\n";
-    }
-
+  Future pickImage() async {
+    final pickedFile = await ImagePicker().pickImage(source: ImageSource.gallery);
     setState(() {
-      resutls;
+      _image = pickedFile != null ? File(pickedFile.path) : null;
+      _output = null;
+      _confidence = 0.0;
+      _predictedName = ""; // Clear previous prediction
     });
   }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  Future<String> getModelPath(String asset) async {
-    final path = '${(await getApplicationSupportDirectory()).path}/$asset';
-    await Directory(dirname(path)).create(recursive: true);
-    final file = File(path);
-    if (!await file.exists()) {
-      final byteData = await rootBundle.load(asset);
-      await file.writeAsBytes(byteData.buffer
-          .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+  Future classifyImage() async {
+    if (_image == null || class_names.isEmpty) { // Check if labels are loaded
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Please select an image and ensure labels are loaded.')),
+      );
+      return;
     }
-    return file.path;
+
+    setState(() {
+      _isClassifying = true;
+    });
+
+    try {
+      final interpreter = await Interpreter.fromAsset('assets/ml/pokemon_model_gemini.tflite');
+      interpreter.allocateTensors();
+
+      final inputShape = interpreter.getInputTensor(0).shape;
+
+      final image = img.decodeImage(File(_image!.path).readAsBytesSync());
+      if (image == null) {
+        throw Exception("Failed to decode image.");
+      }
+
+      final resizedImage = img.copyResize(image, width: 224, height: 224);
+
+      var inputImage = Float32List(1 * inputShape[1] * inputShape[2] * inputShape[3]);
+      int index = 0;
+      for (int i = 0; i < inputShape[1]; i++) {
+        for (int j = 0; j < inputShape[2]; j++) {
+          img.Pixel pixel = resizedImage.getPixel(j, i);
+          inputImage[index++] = pixel.r / 255.0;
+          inputImage[index++] = pixel.g / 255.0;
+          inputImage[index++] = pixel.b / 255.0;
+        }
+      }
+
+      var inputList = inputImage.reshape([1, inputShape[1], inputShape[2], inputShape[3]]);
+
+      var output = List.filled(interpreter.getOutputTensor(0).shape[1], 0.0).reshape(interpreter.getOutputTensor(0).shape);
+
+      interpreter.run(inputList, output);
+
+      setState(() {
+        _output = List<double>.from(output[0]);
+        int maxIndex = _output!.indexOf(_output!.reduce((curr, next) => curr > next ? curr : next));
+        _confidence = _output![maxIndex];
+        _predictedName = class_names[maxIndex]; // Get the name!
+      });
+
+      interpreter.close();
+    } catch (e) {
+      print('Error during classification: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('An error occurred during classification: $e')),
+      );
+    } finally {
+      setState(() {
+        _isClassifying = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Colors.limeAccent.shade400,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text("Recognizer"),
+        title: Text('Pokemon Image Classifier'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
           child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
             mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Card(
-                color: Colors.grey,
-                margin: EdgeInsets.all(16),
-                child: Container(
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 2,
-                  child: image == null
-                      ? Icon(
-                          Icons.image_outlined,
-                          size: 150,
-                        )
-                      : Image.file(image!),
-                ),
+            children: [
+              _image != null
+                  ? Image.file(_image!, height: 200)
+                  : Text('No image selected'),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: pickImage,
+                child: Text('Select Image'),
               ),
-              Card(
-                  margin: EdgeInsets.all(10),
-                  color: Colors.limeAccent.shade400,
-                  child: Container(
-                      height: 100,
-                      child: Row(
-                        children: [
-                          InkWell(
-                            child: Icon(
-                              Icons.image,
-                              size: 50,
-                            ),
-                            onTap: () {
-                              chooseImage();
-                            },
-                          ),
-                          InkWell(
-                            child: Icon(
-                              Icons.camera,
-                              size: 50,
-                            ),
-                            onTap: () {
-                              captureImage();
-                            },
-                          )
-                        ],
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      ))),
-              Card(
-                color: Colors.black,
-                  child: Container(
-                    child: Text(resutls, style: TextStyle(fontSize: 24, color: Colors.white)),
-                    width: MediaQuery.of(context).size.width,
-                    padding: EdgeInsets.all(10),
-                  ),
-                  margin: EdgeInsets.all(10))
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: classifyImage,
+                child: Text('Classify'),
+              ),
+              SizedBox(height: 20),
+              _isClassifying
+                  ? CircularProgressIndicator()
+                  : _output != null
+                  ? Text(
+                'Prediction: $_predictedName (Confidence: ${(_confidence * 100).toStringAsFixed(2)}%)',
+                style: TextStyle(fontSize: 18),
+                textAlign: TextAlign.center,
+              )
+                  : SizedBox.shrink(),
             ],
           ),
         ),
